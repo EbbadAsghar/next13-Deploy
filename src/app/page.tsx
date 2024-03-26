31c60923b633321bf46799416974825129ba89af
EbbadAsghar <ebbadasghar@gmail.com>
2024-03-26 13:16:56 +0500

    useRouter() hook in app/page.tsx for Blog page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,24 @@
+'use client'
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 
 export default function Home() {
+
+  const router = useRouter()
+
   return (
     <div>
       <h2>Hello, I am Ebbad Asghar and this is my first Next.js project.</h2>
+      
+        <button type= "button" onClick={() => router.push('/blog')}>
+          Blog
+        </button>
       <ul>
-        <li><Link href="/blog" replace>Blog</Link></li>
+        {/* <li><Link href="/blog" replace>Blog</Link></li> */}
         <li><Link href="/dashboard">Dashboard</Link></li>
-      </ul>
+      </ul> 
     </div>
   );
 }
